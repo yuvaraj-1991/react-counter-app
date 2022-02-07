a44f5b1dc86553434c6c5a2b91c9dfb99e0a146a
yuvaraj-1991 <yuviatudemy@gmail.com>
2022-02-08 03:29:05 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      count: 0
+    }
+  }
+// Creating methods for Incrementing and Decrementing
+
+  increment = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrement = () => {
+    if(this.state.count <= 0 ){
+      this.state.count = 0
+    } else {
+      this.setState({count: this.state.count - 1})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter</h1>    
+        <div id="value">{this.state.count}</div>    
+        <button onClick={this.increment}>Increase Counter</button>
+        <button onClick={this.decrement}>Decrease Counter</button>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
